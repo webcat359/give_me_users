a8a744c2ef6e18c9620ddc879bc85d3ba7044a75
narakarax <liliyaogerenko@gmail.com>
2022-08-30 06:57:47 -0700

    APP-[8302201]

---
+++ src/components/header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <>
+      <div className="bg-black text-white text-center text-[20px] p-[20px] flex justify-center">
+          <img
+            src="https://photos.angel.co/startups/i/4648183-c5cb80b00cc92d3f8d0414b05ba97955-medium_jpg.jpg?buster=1505378523"
+            alt=""
+          />
+      </div>
+    </>
+  );
+};
+export default Header;
